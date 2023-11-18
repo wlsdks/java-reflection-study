d381449cdcf74009a06c8c8d3db67afd31960ed1
wlsdks <wlsdks12@naver.com>
2023-11-18 17:40:43 +0900

    자바 reflection 실습 프로젝트 추가

---
+++ java-reflection-study/src/main/java/com/example/javareflectionstudy/JavaReflectionStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.javareflectionstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReflectionStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReflectionStudyApplication.class, args);
+	}
+
+}
---
+++ java-reflection-study/src/test/java/com/example/javareflectionstudy/JavaReflectionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javareflectionstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReflectionStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
